a3454fb0287762a9ea4996cba4f88a4b1a736f0e
Anish2504 <anish.kanswal@gmail.com>
2020-02-29 11:41:24 +0100

    added heroku url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 
 
-axios.defaults.baseURL = "https://cooper-api-challenge.herokuapp.com/api/v1";
+axios.defaults.baseURL = "https://cooper-rails-backend.herokuapp.com/api/v1";
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
